93452c8dae6a446106ada4e5dac8a987c060229b
midhuna2331 <midhu8657@gmail.com>
2025-06-09 16:42:42 +0530

    fshey7jk

---
+++ script.js
@@ -0,0 +1,17 @@
+console.log(5==='5');
+let i=0
+do{
+    i++;
+    console.log(i);
+}while(i<5)
+    function greet(name ,age){
+        return 'hello ${name} is ${age} old'
+    }
+    console.log(greet('Ronaldo',40));
+        
+        
+        
+        
+        
+        
+    
